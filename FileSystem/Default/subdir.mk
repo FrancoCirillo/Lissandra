cc278898a51fc4a8bfb34dc23cff3e53d8cea58f
Franco Cirillo <francocirillo3@gmail.com>
2019-04-14 14:59:16 -0300

    Modifico el gitignore

--- FileSystem/Default/subdir.mk
+++ FileSystem/Default/subdir.mk
@@ -4,29 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../hilos.c \
-../tiempo.c \
-../utilsCliente.c \
-../utilsServidor.c 
+../fileSystem.c 
 
 OBJS += \
-./hilos.o \
-./tiempo.o \
-./utilsCliente.o \
-./utilsServidor.o 
+./fileSystem.o 
 
 C_DEPS += \
-./hilos.d \
-./tiempo.d \
-./utilsCliente.d \
-./utilsServidor.d 
+./fileSystem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
