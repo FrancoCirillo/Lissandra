c697ddd4ff7a1eec876ccdf99571c25d69e2db87
Franco <francocirillo3@gmail.com>
2019-04-13 22:58:10 -0300

    Agrego funciones del tiempo
    Copio las funciones que hizo Rodri para mergear. También copié el ejemplo de pthreads para no perderlo.

---
+++ Herramientas/tiempo.h
@@ -0,0 +1,12 @@
+#ifndef HERRAMIENTAS_TIEMPO_H
+#define HERRAMIENTAS_TIEMPO_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+char* obtener_tiempo_cadena();
+time_t obtener_tiempo();
+double segundos_entre(time_t tiempo1,time_t tiempo2);
+
+#endif /* HERRAMIENTAS_TIEMPO_H */
