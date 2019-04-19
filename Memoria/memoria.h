038b8025bb4b55ab8d17a2a9645125b79c8df870
mdiazpereyra <mariano.diaz.pereyra@gmail.com>
2019-04-19 02:43:32 -0300

    API de Memoria y creacion basica de instrucciones

--- Memoria/memoria.h
+++ Memoria/memoria.h
@@ -8,9 +8,9 @@
 #include <commons/config.h>
 #include <commons/string.h>
 #include <readline/readline.h>
-#include "/home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua/Herramientas/utilsCliente.h"
-#include "/home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua/Herramientas/utilsServidor.h"
 #include "/home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua/Herramientas/hilos.h"
+#include "/home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua/Herramientas/definicionesConexiones.h"
+
 
 t_log* g_logger;
 t_config* g_config;
@@ -33,9 +33,29 @@ typedef struct config{
 config configuracion;
 
 
+enum codigos{
+	CODIGO_SELECT = 3,
+	ERROR_SELECT = -3,
+	CODIGO_INSERT = 4,
+	ERROR_INSERT = -4,
+	CODIGO_CREATE = 5,
+	ERROR_CREATE = -5,
+	CODIGO_DESCRIBE = 6,
+	ERROR_DESCRIBE = -6,
+	CODIGO_DROP = 7,
+	ERROR_DROP = -7,
+	CODIGO_JOURNAL = 8,
+	ERROR_JOURNAL = -8
+};
+
+
 void inicializarConfiguracion();
 void loggear(char*);
-char* getByKey(char* ruta, char* key);
+char* getByKey(char*, char*);
+void leerConsola();
+void reconocerRequest(char*, char*, char*, char*, char*);
+instr_t *crearInstruccion(int, char*, char*, char*, char*);
+void print_instruccion(instr_t*);
 
 
 #endif /* MEMORIA_MEMORIA_H_ */
