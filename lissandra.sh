75bcbd8752ecbf60f8fa15f5175eb6146a9b46d7
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2019-06-07 21:00:10 -0300

    Se hace .sh para entrega

--- lissandra.sh
+++ lissandra.sh
@@ -7,3 +7,7 @@ sudo make install
 cd ..
 mkdir /home/utnso/lissandra-checkpoint
 echo "Configuracion inicializada"
+cd /home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua
+make
+echo "Lissandra compilado!"
+echo "Ejecucion lista! 20-04"
