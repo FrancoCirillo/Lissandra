bc91be86ae7cc71d7e5cff4abcf65ae285ef6bb8
Daiana Maidana <daimaidana@est.frba.utn.edu.ar>
2019-06-15 02:49:33 -0300

    Agrego unas funcioncitas en el header, falta revisar como mechar lo que ya existe con esto.

--- FileSystem/compactador.h
+++ FileSystem/compactador.h
@@ -8,6 +8,18 @@
 void compactar();
 void compactador();
 
+
+void actualizar_registros_de_la_particion(char* , t_list* );
+void finalizar_compactacion(char* , t_list* );
+t_list* comparar_registro_a_registro(t_list* , t_list* );
+void filtrar_keys_repetidas(char* , t_list* );
+
+
+//ELiminar
+void compactar2();
+void compactar5();
+
+
 void pasar_a_tmpc(char* tabla);
 
 #endif /* COMPACTADOR_H_ */
