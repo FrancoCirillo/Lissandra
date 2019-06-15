5d6d02781ad98cba97ae878cba2b90df26746b62
Daiana Maidana <daimaidana@est.frba.utn.edu.ar>
2019-06-15 02:47:14 -0300

    Agrego estructura de la compactacion. Logica completa.

--- FileSystem/compactador.c
+++ FileSystem/compactador.c
@@ -1,6 +1,131 @@
 //--------compactador.c--------
 
 #include "compactador.h"
+//
+//void compactador(){// ver que conviene que reciba de parametro
+//	//iniciar con detach a partir del CREATE o de la lectura cuando se inicia el FS.
+//
+//	t_dictionary* reg_x_particion= malloc(sizeof(t_dictionary));
+//
+//	int t = (int)obtener_tiempo_compactacion(tabla); //en segundos
+//
+//	//Esto no se si dejarlo aca o que entre al while, asi no ocupa memoria si es que no hay nada para compactar.. TODO!
+//	int cantidad_particiones = obtener_particiones(tabla); //dev la cantidad de particiones
+//	char* nomb_part;
+//	for(int num = 0; num < cantidad_particiones; num++) {
+//		nomb_part = string_from_format("Part%d", num);
+//		dictionary_put(reg_x_particion, nomb_part, list_create());
+//		free(nomb_part);
+//	} //deja los registros vacios..
+//
+//	void actualizar_registros_de_la_particion(char* particion, t_list* registros_tmpc){
+//			char* bloques= bloques_particion(particion, tabla); // no se si ya existe una funcion similar. TODO
+//			//dev el array.
+//
+//			t_list* registros_bin = leer_registros(bloques);
+//			t_list* registros_nuevos = comparar_registro_a_registro(registros_tmpc, registros_bin);
+//
+//			list_clean(registros_tmpc); //liberar todos los registros viejos, para pisar con los nuevos. Esto hace el free bien?
+//			registros_tmpc = registros_nuevos;
+//			list_destroy(registros_bin);
+//
+//	}
+//
+//	void finalizar_compactacion(char* particion, t_list* registros_tmpc){
+//		//registros_tmpc tiene a esta altura los registros nuevos a escribir.
+//
+//		//Yo escribiria primero los nuevos bloques, despues los libero (para asegurar persistencia)
+//			// El enunciado dice q primero los liberemos..
+//		char* bloques = escribir_nuevos_bloques(registros_tmpc);
+//		//retorna los bloques nuevos que se usaron.
+//
+//		liberar_bloques(particion);
+//
+//		actualizar_bloques_archivo(particion, bloques); //escribe el nuevo array
+//		int nuevo_size = calcular_size(bloques);
+//		actualizar_size_archivo(particion, nuevo_size); //mechar con lo existente.
+//
+//		free(bloques);
+//
+//	}
+//
+//	while(existe_tabla(tabla)){
+//		sleep(t);
+//
+//		//Iniciar cronometro: Enunciado dice que hay que registrar tiempo de la duracion de la compactacion.
+//
+//
+//		if(hay_tmpc(tabla)){
+//			//sem_wait(todos los tmp);
+//			renombrar_todos_tmp_a_tmpc(tabla);
+//			formatear_contador_dump(tabla); //esta ya existe
+//			//sem_post(todos los tmp);
+//		}
+//		else{
+//			continue;
+//		}
+//
+//		char *bloques_a_compactar = bloques_totales_tmpc(tabla); //devuelve el array con todos los num de bloques que debe leer de los tmpc
+//
+//		t_list* registros_totales = leer_registros(bloques_a_compactar); //junta todos los registros de todos los tmpc.
+//
+//		separar_registros_x_part(reg_x_particion, registros_totales);//Segun el modulo de la key y la particion, las agrupa.
+//
+//		dictionary_iterator(reg_x_particion, &filtrar_keys_repetidas);
+//		//filtro para despues tratar cada registro individualmente y preocuparme solo por actualizar o insertar.
+//
+//		dictionary_iterator(reg_x_particion, &actualizar_registros_de_la_particion);
+//		//ya tendria todo en reg_x_particion.
+//
+//		bloquear(tabla);
+//
+//		dictionary_iterator(reg_x_particion, &finalizar_compactacion);
+//
+//		desbloquear(tabla);
+//
+//		vaciar_listas_registros(reg_x_particion);
+//
+//		finalizar_tmpcs(tabla); //Libera bloques, y borra los archivos en el directorio.
+//
+//		//loggear duracion de la compactacion.
+//	}
+//
+//
+//
+//	liberar_recursos(tabla);
+//	//free(de todo lo que use);
+//}
+
+
+t_list* comparar_registro_a_registro(t_list* registros_tmpc, t_list* registros_bin){
+
+	return NULL;
+}
+
+void filtrar_keys_repetidas(char* particion, t_list* registros_tmpc){
+
+
+}
+
+
+
+
+//Para probar detach que no segui intentando..
+void compactar2(){
+	//pthread_create(hilo, tabla, compactar_tabla);
+	while(1){
+		puts("estoy compactando cada 2 segundos\n");
+	}
+}
+
+void compactar5(){
+	//pthread_create(hilo, tabla, compactar_tabla);
+	while(1){
+		puts("estoy compactando cada 5 segundos\n");
+
+	}
+
+}
 
 
 //Hablado el sabado: pseudocodigo
