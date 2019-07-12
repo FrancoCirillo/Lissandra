cefb118917b525f16da575c6d7c7a9996a8609a5
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2019-07-12 19:54:08 -0300

    Se agregan hilos

--- FileSystem/instructions.h
+++ FileSystem/instructions.h
@@ -4,6 +4,10 @@
 #define INSTRUCTIONS_H_
 
 #include "fileSystem.h"
+typedef struct instr_remitente {
+	instr_t* instruccion;
+	char* remitente;
+} instr_remitente;
 
 void 	evaluar_instruccion(instr_t* instr, char* remitente);
 void 	execute_create(instr_t*, char* remitente);
@@ -11,7 +15,7 @@ t_list* execute_insert(instr_t*, cod_op* codOp);
 void 	execute_select(instr_t*, char* remitente);
 void 	execute_describe(instr_t*, char* remitente); //TODO chequear
 void 	execute_drop(instr_t*, char* remitente);
-
+void evaluar_instruccion2(instr_remitente* in) ;
 void 	ejecutar_instruccion_insert(instr_t* instruccion, char* remitente);
 
 void liberar_instruccion(instr_t* instruccion);
