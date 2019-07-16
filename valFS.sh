7755d58bca2f2fe694743e230697766f54be1c0a
Franco Cirillo <francocirillo3@gmail.com>
2019-07-16 20:27:26 -0300

    Subido valFS.sh

--- valFS.sh
+++ valFS.sh
@@ -1,5 +1,8 @@
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"../Herramientas/Debug/"
+cd ..
+cd ..
+rm -rf lfs-base
+cd /home/utnso/git/tp-2019-1c-Como-PCs-en-el-agua
 cd FileSystem
-valgrind Default/FileSystem
-echo "Linda enana"
+valgrind --show-leak-kinds=all --leak-check=full -v --log-file=valgrindFS.txt Default/FileSystem
